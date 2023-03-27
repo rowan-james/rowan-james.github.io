2a5e483a9c6a6f6ab7aac563f3c825396e621ed5
Rowan James <rowan.james.dev@gmail.com>
2023-03-27 17:09:11 -0400

    Deploy to gh-pages

---
+++ _next/static/vGyuQlTDayj-coFUsq-lF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7672b5eae3beeeab.css","static/css/e7fbb07411cd56df.css","static/chunks/pages/index-a063af1202a0d0f8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
