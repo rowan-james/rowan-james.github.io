977d3f85a92c66fba076305aad03b625e677ac94
Rowan James <rowan.james.dev@gmail.com>
2023-03-28 20:10:57 -0400

    change title/desc; use session storage

--- hooks/useSessionStorage.js
+++ hooks/useSessionStorage.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 export function useSessionStorage(key, initialValue) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
-    if (typeof window === "undefined") {
+    if (typeof window === 'undefined') {
       return initialValue
     }
 
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key)
+      const item = window.sessionStorage.getItem(key)
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
@@ -30,8 +30,8 @@ export function useSessionStorage(key, initialValue) {
       // Save state
       setStoredValue(valueToStore)
       // Save to local storage
-      if (typeof window !== "undefined") {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      if (typeof window !== 'undefined') {
+        window.sessionStorage.setItem(key, JSON.stringify(valueToStore))
       }
     } catch (error) {
       // A more advanced implementation would handle the error case
