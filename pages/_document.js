044256028d496b6a52fff779b420dfc7e180a994
Rowan James <rowan.james.dev@gmail.com>
2023-03-30 16:16:13 -0400

    correct github name

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document () {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500&family=Open+Sans:ital,wght@0,300;0,400;1,300;1,400&family=Rajdhani:wght@300;400;500;600&display=swap" rel="stylesheet" />
       </Head>
       <body>
